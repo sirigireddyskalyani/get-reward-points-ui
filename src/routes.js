69b2ac9cde1b9bdbfe209f9a08603e8ecc2d0ead
sirigireddyskalyani <sirigireddyskalyani@gmail.com>
2022-06-18 23:52:23 +0530

    Expenses Table Report Development is Completed 1) Usage of redux is completed. 2) Developed rewards table component to display data. 3) Developed addExpense modal component to accept expense detials. 4) Developed expensesSlice to calculate reward points at action method.

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,19 @@
 import * as React from "react";
 import { Routes, Route } from "react-router-dom";
-import Home from './containers/home';
+import HorizontalNavigator from "./components/navbar";
+import Home from "./containers/home";
 
 function AppNavigator() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
+        <HorizontalNavigator />
+        <div className="container">
+          <div className="min-w-full min-h-full bg-gray-200 p-4">
+            <Routes>
+              <Route path="/" element={<Home />} />
+            </Routes>
+          </div>
+        </div>
     </>
   );
 }
--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
+import expensesReducer from './expensesSlice';
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    expenses: expensesReducer
+  },
 })
